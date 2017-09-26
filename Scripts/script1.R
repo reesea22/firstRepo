deeec3c69fb61b8032cebf0c50d7dd10a19f64cd
Amy Reese <ar22@hood.edu>
2017-09-26 18:45:20 -0400

    Commit changes for HW: added lines 7-21.

--- Scripts/script1.R
+++ Scripts/script1.R
@@ -4,6 +4,21 @@ library(tidyverse)
 #install.packages("cowplot")
 library(cowplot)
 
+# get arguments
+tmp <- commandArgs(TRUE)
+
+# make a named list of arguments
+lst <- strsplit(tmp, '=', fixed = TRUE) %>%
+  lapply(`[`, 2)
+
+names(lst) <- strsplit(tmp, '=', fixed = TRUE) %>%
+  sapply(`[`, 1)
+# fill in defaults if not given
+if(is.null(lst$out))
+  lst$out <- 'out1.pdf'
+#Also change the call to pdf() such that it uses our argument:
+  #pdf(lst$out)
+
 set.seed(297834) # pick your own seed
 dat <- data_frame(pred = rnorm(100),
                   resp = pred + rnorm(100))
